5fe9082f2ce4847aa61435232c7f16364ae604b7
DerianSeto <144382635+DerianSeto@users.noreply.github.com>
2023-10-11 17:45:55 -0600

    Updated to remove disabling the button
    Disabling the button was done because the menu would glitch about in place before. I figured it was Up() and Down() being called at the same time so I disabled the button to mitigate

--- Dropdown.js
+++ Dropdown.js
@@ -1,23 +1,28 @@
 //Global variables
-let increment = 0.0;
+let animating = false;												//Use instead of disabling button
+let increment = -5.1;
 let dropped = false;
 
 function Dropdown(){
-	//HTML objects-Navigation menu, Navigation button
-	var Drop = document.getElementById('Drop');
-	document.getElementById('Dropbtn').disabled = true;
-	
-	if(dropped){ //Checks if it is opened or closed
-		up = setInterval(Up, 1);									//Starts loop with time
-	}else{
-		down = setInterval(Down, 1);								//Starts loop with time
-	}
+	//alert("AAA");
+	do { //setInterval is asynchronous, so will have to redo								//Do...while doesn't work
+		//alert("AAA");
+		if(!animating){ //Checks if the the menu is opening or closing
+			if(dropped){ //Checks if it is opened or closed
+				animating = true;										//Says that the menu is animating
+				up = setInterval(Up, 1);								//Starts loop with time
+			}else{
+				animating = true;										//Says that the menu is animating
+				down = setInterval(Down, 1);							//Starts loop with time
+			}
+		}
+	}while(animating);
 }	
 
 
 function Read(){//Puts information in an alert						//Enable button from the HTML file
 	var Drop = document.getElementById('Drop');
-	alert(Drop.style.height);
+	//alert(Drop.style.height);
 	alert(increment);
 }
 
@@ -26,10 +31,11 @@ function Down(inc){	//Opens the menu
 	increment = increment + 0.1;									//Increments the X value of logistic sigmoid function
 	Drop.style.height = (150/(1 + Math.pow(2.718, -increment)));	//Sets navigation menu height
 
-	if(parseInt(Drop.style.height)>=130){
+	if(parseInt(Drop.style.height)>=140){
 		clearInterval(down);										//Ends loop
 		dropped = true;												//Tells Dropdown() that the menu is open
-		document.getElementById('Dropbtn').disabled = false;		//Enables button again
+		//document.getElementById('Dropbtn').disabled = false;		//Enables button again
+		animating = false;
 	}
 }
 
@@ -41,7 +47,7 @@ function Up(inc){ //Closes the menu
 	if(parseInt(Drop.style.height)<=0){
 		clearInterval(up);											//Ends loop
 		dropped = false;											//Tells Dropdown() that the menu is closed
-		increment = 0.0;											//Resets (global) increment
-		document.getElementById('Dropbtn').disabled = false;		//Enables button again
+		//document.getElementById('Dropbtn').disabled = false;		//Enables button again
+		animating = false;											//Says that the menu is no longer animating
 	}
 }
