121dc0bd499f9f762bb6b884d143bff1cf22d61d
DerianSeto <144382635+DerianSeto@users.noreply.github.com>
2023-10-12 13:08:38 -0600

    Fixed it from last time.
    Removed disabling the button, created a loop in case of double click. It will now call again once in the cycle if it is clicked while still animating. Took some time too

--- Dropdown.js
+++ Dropdown.js
@@ -1,53 +1,60 @@
 //Global variables
-let animating = false;												//Use instead of disabling button
 let increment = -5.1;
 let dropped = false;
+let Go;
 
-function Dropdown(){
-	//alert("AAA");
-	do { //setInterval is asynchronous, so will have to redo								//Do...while doesn't work
-		//alert("AAA");
+//Use instead of disabling button
+let animating = false;
+let requestedDrop = false;
+
+
+function Read(){ //Puts information in an alert										//Enable button from the HTML file
+	let Drop = document.getElementById('Drop');
+	alert(Drop.style.height);
+	alert(dropped);
+}
+
+
+function Dropdown(){ //Called by Dropdown button
+	let Drop = document.getElementById('Drop');										//Gets Navigation menu HTML element
 		if(!animating){ //Checks if the the menu is opening or closing
-			if(dropped){ //Checks if it is opened or closed
-				animating = true;										//Says that the menu is animating
-				up = setInterval(Up, 1);								//Starts loop with time
+			animating = true;														//Says that the menu is animating
+			
+			if(!dropped){ //Checks if it is opened or closed
+				Go = setInterval(function(){ //Starts loop with time
+					increment = increment + 0.1;									//Increments
+					Drop.style.height = (150/(1 + Math.pow(2.718, -increment)));	//Sets navigation menu height
+					
+					if(parseInt(Drop.style.height)>=130){ //Checks for end
+						End();														//Ends loop
+						dropped = true;												//Tells Dropdown() that the menu is open
+					}
+				}, 1);
+				
 			}else{
-				animating = true;										//Says that the menu is animating
-				down = setInterval(Down, 1);							//Starts loop with time
+				Go = setInterval(function(){ //Starts loop with time
+					increment = increment - 0.1;									//Increments
+					Drop.style.height = (150/(1 + Math.pow(2.718, -increment)));	//Sets navigation menu height
+					
+					if(parseInt(Drop.style.height)<=0){ //Checks for end
+						End();														//Ends loop
+						dropped = false;											//Tells Dropdown() that the menu is open
+					}
+				}, 1);
+				
 			}
+		}else{
+			requestedDrop = true;
 		}
-	}while(animating);
 }	
 
-
-function Read(){//Puts information in an alert						//Enable button from the HTML file
-	var Drop = document.getElementById('Drop');
-	//alert(Drop.style.height);
-	alert(increment);
-}
-
-function Down(inc){	//Opens the menu
-	var Drop = document.getElementById('Drop');						//Gets Navigation menu HTML element
-	increment = increment + 0.1;									//Increments the X value of logistic sigmoid function
-	Drop.style.height = (150/(1 + Math.pow(2.718, -increment)));	//Sets navigation menu height
-
-	if(parseInt(Drop.style.height)>=140){
-		clearInterval(down);										//Ends loop
-		dropped = true;												//Tells Dropdown() that the menu is open
-		//document.getElementById('Dropbtn').disabled = false;		//Enables button again
-		animating = false;
-	}
-}
-
-function Up(inc){ //Closes the menu
-	var Drop = document.getElementById('Drop');						//Gets Navigation menu HTML element
-	increment = increment - 0.1;									//Increments the X value of logistic sigmoid function
-	Drop.style.height = (150/(1 + Math.pow(2.718, -increment)));	//Sets navigation menu height
+function End(){//Ends loop
+	clearInterval(Go);																//Ends loop
+	animating = false;																//Tells Dropdown() that the menu is animating
 	
-	if(parseInt(Drop.style.height)<=0){
-		clearInterval(up);											//Ends loop
-		dropped = false;											//Tells Dropdown() that the menu is closed
-		//document.getElementById('Dropbtn').disabled = false;		//Enables button again
-		animating = false;											//Says that the menu is no longer animating
+	if(requestedDrop){ //To go again if button is reclicked while animating | Only once per cycle
+		Dropdown();
+	}else{
+		requestedDrop = false;
 	}
 }
